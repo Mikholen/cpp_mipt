f34b7376af7ce8033ee1db628bd4c50e6337c328
mikholen <mikhalinakharkevich@mail.ru>
2025-09-28 22:08:48 +0000

    bine proof

--- 02/02_01.cpp
+++ 02/02_01.cpp
@@ -4,10 +4,11 @@
 const double phi = (1 + std::sqrt (5)) / 2;
 
 int fibonacci_number_calculator (int N);
+int fibonacci_number_calculator_eval (int N);
 
 int main () {
 
-    int N = NAN;
+    int N = 0;
 
     std::cin >> N;
 
@@ -19,6 +20,7 @@ int main () {
     // std::cout << '\n';
 
     std::cout << fibonacci_number_calculator (N) << '\n';
+    std::cout << "Correct: " << fibonacci_number_calculator_eval (N) << '\n';
 
     return 0;
 }
@@ -30,4 +32,17 @@ int fibonacci_number_calculator (int N) {
     number = (std::pow (phi, N) - std::pow (-phi, -N)) / (2 * phi - 1);
 
     return static_cast<int>(number);
+}
+
+int fibonacci_number_calculator_eval (int N) {
+
+    if ((N == 1) || (N == 2)) {
+
+        return 1;
+    }
+
+    else {
+
+        return fibonacci_number_calculator_eval (N - 2) + fibonacci_number_calculator_eval (N - 1);
+    }
 }
