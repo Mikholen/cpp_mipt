2e3761e31860313da57a81c4b2b5df9c096adbc3
Mikhalina <mikhalinakharkevich@mail.ru>
2025-12-10 18:40:13 +0300

    04_05 final

---
+++ 04/04_05.cpp
@@ -0,0 +1,32 @@
+#include <limits>
+
+template <int X> struct Fibonacci {
+
+    int const n = X;
+    static inline int const value = Fibonacci < X - 1 > ::value + Fibonacci < X - 2 > ::value; 
+    static_assert (value >= std::numeric_limits<int>::min() && value <= std::numeric_limits<int>::max());
+};
+
+template <>      struct Fibonacci <1> {
+
+    int const n = 1;
+    static inline int const value = 1;
+    static_assert (value >= std::numeric_limits<int>::min() && value <= std::numeric_limits<int>::max());
+};
+
+template <>      struct Fibonacci <2> {
+
+    int const n = 2;
+    static inline int const value = 1;
+    static_assert (value >= std::numeric_limits<int>::min() && value <= std::numeric_limits<int>::max());
+};
+
+template <int X>
+auto const F_val = Fibonacci <X> ::value;
+
+int main () {
+
+    static_assert (F_val <3> == 2);
+    static_assert (F_val <5> == 5);
+    static_assert (F_val <7> == 13);
+}
