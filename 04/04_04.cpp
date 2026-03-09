e460298bcb3027095b07beaf4dc8d381e86c438a
Mikhalina <mikhalinakharkevich@mail.ru>
2026-03-09 12:33:01 +0300

    final

--- 04/04_04.cpp
+++ 04/04_04.cpp
@@ -1,144 +1,239 @@
-#include <algorithm>
-#include <cstddef>
-#include <initializer_list>
-#include <iterator>
-#include <utility>
-#include <iostream>
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <cassert>
+#include <cmath>
+#include <istream>
+#include <numeric>
+#include <ostream>
+#include <sstream>
+#include <vector>
+#include <compare>
+
+///////////////////////////////////////////////////////////////////////////////////////////////
 
 template <typename T>
-class Vector {
-
+class Rational
+{
 public :
 
-	Vector () : m_array (nullptr), m_size (0) {
-
-		std::cout << "Vector:: Vector (1)" << '\n';
+	Rational(T num = 0, T den = 1) : m_num(num), m_den(den)
+	{
+		reduce();
 	}
 
-	Vector (std::initializer_list < T > list) : m_size (std::size (list)) {
+//  -------------------------------------------------------------------------------------------
 
-		std::cout << "Vector:: Vector (2)" << '\n';
-
-		m_array = m_size ? new T[m_size]{} : nullptr;
-
-		std::ranges::copy (list, m_array);
+	explicit operator double() const
+	{ 
+		return 1.0 * m_num / m_den;
 	}
 
-	Vector (Vector const & other) : m_size (other.m_size) {
+//  -------------------------------------------------------------------------------------------
 
-		std::cout << "Vector:: Vector (3)" << '\n';
+	auto & operator+=(Rational const & other)
+	{
+		auto lcm = std::lcm(m_den, other.m_den);
 
-		m_array = m_size ? new T[m_size]{} : nullptr;
-
-		std::ranges::copy (other.m_array, other.m_array + other.m_size, m_array);
-	}
-
-	Vector (Vector && other)
-	: 
-		m_array (std::exchange(other.m_array, nullptr)), 
+		m_num = m_num * (lcm / m_den) + other.m_num * (lcm / other.m_den);
 		
-		m_size  (std::exchange(other.m_size,  0      )) {
+		m_den = lcm;
 
-		std::cout << "Vector:: Vector (4)" << '\n';
-	}
-
-   ~Vector () {
-
-		std::cout << "Vector::~Vector" << '\n';
-
-		delete[] m_array;
-	}
-
-	auto & operator=(Vector other) {
-
-		std::cout << "Vector::operator= (4)" << '\n';
-
-		swap (other);
+		reduce();
 
 		return *this;
 	}
 
-	void swap (Vector & other) {
+//  -------------------------------------------------------------------------------------------
 
-		std::swap (m_array, other.m_array);
-		
-		std::swap (m_size,  other.m_size );
+	auto & operator-=(Rational const & other) 
+	{ 
+		return *this += Rational(other.m_num * -1, other.m_den);
 	}
 
-    std::size_t size () {
+//  -------------------------------------------------------------------------------------------
 
-        return m_size;
+	auto & operator*=(Rational const & other)
+	{
+		m_num *= other.m_num;
+
+		m_den *= other.m_den;
+
+		reduce();
+
+		return *this;
+	}
+
+//  -------------------------------------------------------------------------------------------
+	
+	auto & operator/=(Rational const & other) 
+	{ 
+		return *this *= Rational(other.m_den, other.m_num);
+	}
+
+//  -------------------------------------------------------------------------------------------
+
+	auto const operator++(T) { auto x = *this; *this += 1; return x; }
+
+	auto const operator--(T) { auto x = *this; *this -= 1; return x; }
+
+//  -------------------------------------------------------------------------------------------
+
+	auto & operator++() { *this += 1; return *this; }
+
+	auto & operator--() { *this -= 1; return *this; }
+
+//  -------------------------------------------------------------------------------------------
+
+	friend auto operator+ (Rational lhs, Rational const & rhs) { return lhs += rhs; }
+
+	friend auto operator- (Rational lhs, Rational const & rhs) { return lhs -= rhs; }
+
+	friend auto operator* (Rational lhs, Rational const & rhs) { return lhs *= rhs; }
+
+	friend auto operator/ (Rational lhs, Rational const & rhs) { return lhs /= rhs; }
+
+//  -------------------------------------------------------------------------------------------
+
+    friend std::strong_ordering operator<=>(const Rational& lhs, const Rational& rhs) {
+
+        T lhs_cross = lhs.m_num * rhs.m_den;
+        T rhs_cross = rhs.m_num * lhs.m_den;
+
+        if (lhs_cross < rhs_cross)
+
+            return std::strong_ordering::less;
+
+        else if (lhs_cross > rhs_cross)
+
+            return std::strong_ordering::greater;
+
+        else
+
+            return std::strong_ordering::equal;
     }
 
-    std::size_t capacity () {
+    friend bool operator==(const Rational& lhs, const Rational& rhs) {
 
-        return m_capacity;
+        return (lhs <=> rhs) == std::strong_ordering::equal;
+
     }
 
-    void push_back (const T element) {
 
-        if (m_size + 1 > m_capacity) {
+//  -------------------------------------------------------------------------------------------
 
-            m_capacity = (m_capacity == 0) ? 1 : m_capacity * 2;
-            auto new_arr = new T[m_capacity]{};
-            std::ranges::copy (m_array, m_array + m_size, new_arr);
+	friend auto & operator>>(std::istream & stream, Rational & rational)
+	{
+		return (stream >> rational.m_num).ignore() >> rational.m_den;
+	}
 
-            delete[] m_array;
-            m_array = new_arr;
-            m_array[m_size] = element;
+//  -------------------------------------------------------------------------------------------
 
-            m_size++;
-        }
-
-        else {
-
-            m_array[m_size] = element;
-            m_size++;
-        }
-    }
-
-    bool empty () {
-
-        return (m_size == 0);
-    }
-
-    void clear () {
-
-        for (std::size_t i = 0; i < m_size; i++) {
-
-            m_array[i] = 0;
-        }
-
-        m_size = 0;
-    }
+	friend auto & operator<<(std::ostream & stream, Rational const & rational)
+	{
+		return stream << rational.m_num << '/' << rational.m_den;
+	}
 
 private :
 
-	T * m_array = nullptr;
+	void reduce()
+	{
+		if (m_den < 0)
+		{
+			m_num = -m_num;
 
-	std::size_t m_size     = 0;
-    std::size_t m_capacity = 0;
+			m_den = -m_den;
+		}
+
+		auto gcd = std::gcd(m_num, m_den);
+
+		m_num /= gcd;
+
+		m_den /= gcd;
+	}
+
+	T m_num = 0, m_den = 1;
 };
 
-void tests () {
-
-    Vector<int> vector_1;
-
-	Vector vector_2 = { 1, 2, 3, 4, 5 };
-
-	Vector vector_3 = vector_2;
-
-	Vector vector_4 = std::move (vector_3);
-
-	Vector vector_5 = { 2.7, 8. , 9.0, 5.4 };
-
-	vector_3 = vector_2;
-
-	vector_4 = std::move (vector_3);
-
+auto equal(double x, double y, double epsilon = 1e-6)
+{
+	return std::abs(x - y) < epsilon;
 }
 
-int main () {
+int main()
+{
+	Rational x = 1, y(2, 1);
 
-	tests ();
+//	std::vector < int > vector_1 = 5; // error
+
+	std::vector < int > vector_2(5);
+
+	std::vector < int > vector_3 = { 1, 2, 3, 4, 5 };
+
+	assert (equal(static_cast < double > (x), 1));
+
+//	assert (x.operator+=(y) == Rational(3, 1)); // bad
+
+	assert ((x += y) == Rational(+3, 1));
+
+	assert ((x -= y) == Rational(+1, 1));
+
+	assert ((x *= y) == Rational(+2, 1));
+
+	assert ((x /= y) == Rational(+1, 1));
+
+	assert ((x ++  ) == Rational(+1, 1)); // support : compiler-explorer.com
+
+	assert ((x --  ) == Rational(+2, 1));
+
+	assert ((  ++ y) == Rational(+3, 1)); // support : compiler-explorer.com
+
+	assert ((  -- y) == Rational(+2, 1));
+
+//	x++++; // error
+
+	[[maybe_unused]] auto z = 0;
+
+//	z++++; // error
+
+//	assert (operator+(x, y) == Rational(3, 1)); // bad
+
+	assert ((x +  y) == Rational(+3, 1));
+
+	assert ((x -  y) == Rational(-1, 1));
+
+	assert ((x *  y) == Rational(+2, 1));
+
+	assert ((x /  y) == Rational(+1, 2));
+
+	assert ((x += 1) == Rational(+2, 1));
+
+//	assert ((1 += x) == Rational(+3, 1)); // error
+
+	assert ((x +  1) == Rational(+3, 1));
+
+	assert ((1 +  y) == Rational(+3, 1));
+
+	assert ((1 +  1) == Rational(+2, 1));
+
+	assert ((x <  y) == 0);
+	
+	assert ((x >  y) == 0);
+
+	assert ((x <= y) == 1);
+
+	assert ((x >= y) == 1);
+
+	assert ((x == y) == 1);
+
+	assert ((x != y) == 0);
+
+	std::stringstream stream_1("1/2");
+
+	std::stringstream stream_2;
+
+	stream_1 >> x;
+
+	stream_2 << x;
+
+	assert (stream_2.str() == stream_1.str());
 }
